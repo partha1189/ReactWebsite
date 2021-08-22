7b01333ef2c98dbb86dc602868aa695a334ee697
Partha Sen <partha1189@gmail.com>
2021-08-22 19:51:18 +0530

    intro

---
+++ src/components/AnimatedShapes.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const Square = styled.div`
+  width: 60px;
+  height: 60px;
+  background-color: #4e6bff;
+  opacity: 0.7;
+  position: absolute;
+  top: -60px;
+  left: -60px;
+  z-index: -1;
+  animation: square 25s linear alternate infinite;
+
+  @keyframes square {
+    to {
+      transform: translate(100vw, 100vh);
+    }
+  }
+`;
+
+const AnimatedShapes = () => {
+  return <Square />;
+};
+
+export default AnimatedShapes;
